726a63c1a8b030f8f2385154491c506ce108905e
Liiner97 <coelhitohg@gmail.com>
2024-11-06 16:06:07 +0000

    Time

--- Assets/Script/SecondsUpdate.cs
+++ Assets/Script/SecondsUpdate.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SecondsUpdate : MonoBehaviour
-{
+{      
+    public float moveSpeed = 0.5f;
+    float timeStartOffset = 0;
+    bool gotStartTime = false;
     void Update()
     {
-
+        if (!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, (Time.realtimeSinceStartup - timeStartOffset) * moveSpeed);
     }
 }
